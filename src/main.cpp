fdca3623fbdad2168304eb030267c4c8d4bcf964
CshardZ <vivekhubstrt@gmail.com>
2026-07-11 09:39:46 +0530

    feat: first hello world app

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,10 @@
-#include "app-window.h"
+#include "index.h"
 
-#ifdef __ANDROID__
-extern "C" void slint_main()
-#else
-int main(int argc, char **argv)
-#endif
+int main()
 {
-    auto ui = AppWindow::create();
-
-    ui->on_request_increase_value([&]{
-        ui->set_counter(ui->get_counter() + 1);
-    });
+    auto ui = HelloWorld::create();
 
     ui->run();
-#ifndef __ANDROID__
+
     return 0;
-#endif
 }
