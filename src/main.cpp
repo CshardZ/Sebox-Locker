3c0cb6b18b9d91014b41cc3592f0016d22d104f5
CshardZ <vivekhubstrt@gmail.com>
2026-07-12 19:37:55 +0530

    feat: add native nfd file selector dialogue box

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "index.h"
+#include <nfd.h>
 
-int main()
-{
-    auto ui = HelloWorld::create();
+
+int main() {
+    auto ui = RootWindow::create();
+
+    ui->on_addFiles([]() {
+        NFD_Init();
+        nfdchar_t *outPath = NULL;
+        nfdresult_t result = NFD_OpenDialog(&outPath, NULL, 0, NULL);
+
+        if (result == NFD_OKAY) {
+            // Do your logic here
+            NFD_FreePath(outPath);
+        }
+        NFD_Quit();
+    });
 
     ui->run();
-
     return 0;
 }
