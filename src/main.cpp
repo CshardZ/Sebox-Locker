8f74f9d0864526edf73685ea2c789b5df2c39833
CshardZ <vivekhubstrt@gmail.com>
2026-07-14 19:23:38 +0530

    feat(core): configure platform specific appdata and add files/folder functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,74 @@
 #include <index.h>
 #include "file/file_picker.h"
 
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+namespace fs = std::filesystem;
+
+fs::path GetAppStorageDir() {
+    /* 
+    - Create and/or Get
+    - platform specific app data directory path
+    */
+    fs::path base;
+    #if defined(_WIN32)
+        // Windows: Use LocalAppData environment variable
+        const char* appdata = std::getenv("LOCALAPPDATA");
+        base = appdata ? fs::path(appdata) : fs::path(".");
+    #elif defined(__APPLE__)
+        // macOS: Standard user library path
+        base = fs::path(std::getenv("HOME")) / "Library/Application Support";
+    #else
+        // Linux: Follows XDG standard
+        const char* home = std::getenv("HOME");
+        base = home ? (fs::path(home) / ".local/share") : fs::path(".");
+    #endif
+
+    fs::path seboxAppDataDir = base / "Sebox";
+    std::error_code ec;
+    if (!fs::exists(seboxAppDataDir)) {
+        if (!fs::create_directories(seboxAppDataDir, ec)) {
+            std::cerr << "Failed to create directory: " << ec.message() << std::endl;
+        }
+    }
+    std::cout <<"Created/Retrieved App data path: " << seboxAppDataDir << std::endl;
+    return seboxAppDataDir;
+}
+
 
 int main() {
     auto ui = RootWindow::create();
+
     
     FilePicker picker; 
     ui->on_open_nfd_files_selector([&picker]() {
         std::cout << "Add Files - button clicked" << std::endl;
-        picker.pickFiles();
+        fs::path seboxAppDataDir = GetAppStorageDir(); // create and/or get
+        std::vector<std::string> files = picker.pickFiles();
+        if(!files.empty()) {
+            for (const auto& pathStr : files) {
+                fs::path source = pathStr;
+                fs::copy(source, seboxAppDataDir / source.filename());
+            }
+        }
+        std::cout<<"Files Have been copied to appdata directory"<<std::endl;
     });
-
+    
     ui->on_open_nfd_folder_selector([&picker]() {
         std::cout <<"Add Folder - button clicked" << std::endl;
-        picker.pickFolder();
+        fs::path seboxAppDataDir = GetAppStorageDir(); // create and/or get
+        std::string folder = picker.pickFolder();
+        if(!folder.empty()) {
+            fs::path source = folder;
+            fs::copy(
+                source, seboxAppDataDir / source.filename(), 
+                fs::copy_options::recursive | fs::copy_options::overwrite_existing
+            );
+        }
+        std::cout<<"Folder has been copied to appdata directory"<<std::endl;
     });
 
     ui->run(); 
